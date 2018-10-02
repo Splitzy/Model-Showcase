d1e0d0d8bfd216281443b70e13fe6449c7381413
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-10-02 17:47:51 +0100

    Workshop 2

---
+++ Design Challenge 1/Assets/Code/DoorOpen.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen : MonoBehaviour {
+
+    Animator anim;
+    public bool doorOpen = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetKeyDown("e"))
+        {
+            OpenDoor();
+        }
+	}
+
+    void OpenDoor()
+    {
+        if (doorOpen == true)
+        {
+            //anim.Play();
+            doorOpen = false;
+        }
+        else
+        {
+            //anim.Play();
+            doorOpen = true;
+        }
+    }
+}
