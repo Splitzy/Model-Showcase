f2b895bcd9cf4e7daecfb6ced470120285368247
Alex "Splitz" Davis <32062643+Splitzy@users.noreply.github.com>
2018-11-03 20:00:13 +0000

    Added Sounds for the unity scenes

---
+++ Design Challenge 1 Folder/Design Challenge 1/Assets/Code/Footsteps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+
+    CharacterController cc;
+    AudioSource audio;
+
+	// Use this for initialization
+	void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		if(cc.isGrounded == true && cc.velocity.magnitude > 2f && audio.isPlaying == false)
+        {
+            audio.volume = Random.Range(0.8f, 1f);
+
+            //audio.play();
+        }
+	}
+}
